2d6432ce981947eb46e597784aedf4de9bc6d4c1
arerickson28 <a.r.erickson.28@gmail.com>
2024-08-07 20:05:32 -0500

    begin WIP

---
+++ kcat-commands/src/components/SslLoc.vue
@@ -0,0 +1,37 @@
+
+
+
+<template>
+    <div class = "tealBox">
+
+        <h3>Your SSL Location</h3>
+        <div class ="partsDiv">
+        <h3>ssl.certificate.location = changeMe</h3>
+        <h3>ssl.key.location = changeMe</h3>
+        <h3>ssl.ca.location = changeMe</h3>
+        </div>
+        <button>Update</button>
+        <hr></hr>
+
+    </div>
+ 
+      
+</template>
+
+
+<style>
+.tealBox {
+    border: solid 4px rgb(0,128,128);
+    padding: 5px;
+    margin: 10px;
+    /* display: flex;
+    flex-direction: column;
+    align-items: center; */
+    /* justify-content: center; */
+}
+
+.partsDiv {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
---
+++ kcat-commands/src/components/Topic.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  data() {
+    return {
+      message: 'kcat -P -X security.protocol=ssl -X ssl.certificate.location=<changeMe> -X ssl.key.location=<changeMe> -X ssl.ca.location=<changeMe> -b awsomeStageServer:9092 -t my-awesome-topic'
+    }
+  }
+}
+
+</script>
+
+<template>
+    <p class="purple">{{ message }}</p>
+</template>
+
+
+<style>
+.purple {
+    border: solid 4px rgb(128, 17, 128);
+    padding: 5px;
+    margin: 10px;
+    &:hover {
+        box-shadow: inset 0px 0px 8px 
+        purple, 0 0 15px purple ;
+    }
+}
+</style>
